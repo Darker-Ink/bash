ecb530d91adf434c7a0ffa3520842ad5f6fcd9e5
DarkInk <rstonehouse66@gmail.com>
2022-09-26 01:14:23 +0000

    Fixing Something

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+sleep 3
+
 cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-
-# Echos Startup Variables
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
